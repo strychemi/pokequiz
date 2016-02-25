803e5b2d3b4c900388430bacafe0a08c6f18cc16
Kelsey James <kelseybjames@gmail.com>
2016-02-25 11:44:42 -0800

    added token generation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,16 @@ class User < ActiveRecord::Base
             :length => { :in => 8..24 }, 
             :allow_nil => true
   validates :email, presence: true
+
+  def generate_token
+    begin
+    self[:auth_token] = SecureRandom.urlsafe_base64
+    end while User.exists?(auth_token: self[:auth_token])
+  end
+
+  def regenerate_auth_token
+    self.auth_token = nil
+    generate_token
+    save!
+  end
 end
