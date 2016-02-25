d48eebcce02fb6ca7c94984cdbfba211489ae1a8
Andrew Baik <and.baik@gmail.com>
2016-02-25 16:44:04 -0500

    setting up seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,57 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# ----------------------------------------
+# Database Reset
+# ----------------------------------------
+
+if Rails.env == 'development'
+  puts 'Reseting database'
+
+  Rake::Task['db:migrate:reset'].invoke
+end
+
+# ----------------------------------------
+# Seed Config Vars
+# ----------------------------------------
+
+MULTIPLIER = 10
+TYPES = 2
+
+# ----------------------------------------
+# Setup API
+# ----------------------------------------
+
+@pokeapi = PokeAPI.new
+
+# ----------------------------------------
+# Make API calls for Type
+# ----------------------------------------
+
+puts "Making Type API calls"
+
+@pokeapi.get_all_types
+
+# ----------------------------------------
+# Populate database with Types
+# ----------------------------------------
+
+puts "Populating database with types"
+
+all_types = @pokeapi.types
+
+(1..TYPES).each do |x|
+  Type.create(name: all_types[x][:name] )
+end
+
+# ----------------------------------------
+# Populate database with Type relationships
+# ----------------------------------------
+
+puts "Populating type_relationships with default normal effectiveness"
+(1..TYPES).each do |attack|
+  (1..TYPES).each do |defend|
+    TypeRelationship.create(attack_type_id: attack, defend_type_id: defend, effectiveness: "normal" )
+  end
+end
