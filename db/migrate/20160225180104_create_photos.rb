d4bd389b15cc4f672a364eb8f79c9711db6e99d2
Kelsey James <kelseybjames@gmail.com>
2016-02-25 10:59:12 -0800

    added paperclip, database solid

--- db/migrate/20160225180104_create_photos.rb
+++ db/migrate/20160225180104_create_photos.rb
@@ -1,7 +1,7 @@
 class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
-      t.attachment :photo
+      t.attachment :photo, null: false
 
       t.timestamps null: false
     end
