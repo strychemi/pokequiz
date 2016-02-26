9a3ddf7c328996fb6625e45313ad141655d63e1a
Joshua Masland-Sarani <j.masland@gmail.com>
2016-02-25 22:33:52 -0500

    add factories and tests for user model

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :profile do
     sequence(:first_name) { |n| "First#{n}" }
     sequence(:last_name) { |n| "Last#{n}" }
-    sequence(:username) { |n| "firstlast"}
+    sequence(:username) { |n| "firstlast#{n}"}
     sequence(:user_id) { |n| n }
     pokemon_id(1)
     type_id(1)
