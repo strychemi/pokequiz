e0c1dc92fca163c22e933a93a506995451e69405
Jeffrey Gisin <jgisin@gmail.com>
2016-02-25 19:15:15 -0700

    email works

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,6 +22,7 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
   config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
